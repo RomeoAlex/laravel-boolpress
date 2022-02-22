536ab1ff9a1b3261492bd3f082fe8fd81390cf9d
RomeoAlex <romeo.alessandro17@gmail.com>
2022-02-22 23:29:12 +0100

    funzione per lo slug

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 // richiamo il model
 use App\Post;
-
+// per slug bisogna importare la funzione di supporto
+use Illuminate\Support\Str;
 class PostController extends Controller
 {
     /**
@@ -49,10 +50,35 @@ class PostController extends Controller
     {
         $form_data = $request->all();
         // validazione per i campi vuoti
-        $request->validate([
-            'title' => 'required|max:255',
-            'content' => 'required|max:60000'
-        ]);
+        // $request->validate([
+        //     'title' => 'required|max:255',
+        //     'content' => 'required|max:60000'
+        // ]);
+        // se uso la funzione scritta al fondo 
+        $request->validate($this->getValidateRules());
+            // creo le variabili per salvare i dati ricevuti dal form
+        $new_post = new Post();
+        $new_post->fill($form_data);
+        // ma devo popolare lo slug utilizzando la sua sintassi e il titolo del form
+        // $new_post->slug = Str::slug( $form_data['title'] );
+        // inoltre controllo se lo slug esiste e aggiungo man mano 1 finche non esiste uno slug univoco
+        // POSTO LA FUNZIONE ALLA FINE PER RIUTILIZZARLA NELL'EDIT
+        // $slug = Str::slug( $form_data['title'] );
+        //     // per non creare concatenzioni nel while
+        // $slug_base = $slug;
+        // $post_found = Post::where('slug', '=', $slug)->first();
+        // $counter = 1;
+        // while ($post_found) {
+        //     // se esiste agiungiamo un - , $counter che man mano inceremnterà, al titolo
+        //     $slug = $slug_base . '-' . $counter;
+        //     // dovrà avanti finche il valore non sarà vuoto , null e perciò poterlo inserire nel database
+        //     $post_found = Post::where('slug', '=', $slug)->first();
+        //     $counter++;
+        // }
+        $new_post->slug = $this->getUniqueSlug($form_data['title']);
+        $new_post->save();
+        // ritorno alla pagina del post creato
+        return  redirect()->route('admin.posts.show', ['post' => $new_post->id]);
     }
 
     /**
@@ -106,5 +132,28 @@ class PostController extends Controller
     public function destroy($id)
     {
         //
+    }
+    // creo funzione per la validazione
+    protected function getValidateRules(){
+        return [
+            'title' => 'required|max:255',
+            'content' => 'required|max:60000'
+        ];
+    }
+    protected function getUniqueSlug($title){
+        $slug = Str::slug($title);
+            // per non creare concatenzioni nel while
+        $slug_base = $slug;
+        $post_found = Post::where('slug', '=', $slug)->first();
+        $counter = 1;
+        while ($post_found) {
+            // se esiste agiungiamo un - , $counter che man mano inceremnterà, al titolo
+            $slug = $slug_base . '-' . $counter;
+            // dovrà avanti finche il valore non sarà vuoto , null e perciò poterlo inserire nel database
+            $post_found = Post::where('slug', '=', $slug)->first();
+            $counter++;
+        }
+        // riportiamo il valore slug
+        return $slug;
     }
 }
