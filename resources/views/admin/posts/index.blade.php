0aef49c829d70a2cdcaf950f7091454455a29594
RomeoAlex <romeo.alessandro17@gmail.com>
2022-02-22 19:40:37 +0100

    impementate cards

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,7 +4,24 @@
     <section>
         <div class="container">
             <h1>Lista post</h1>
-            {{ dd($posts)}}
+            <div class="row row-cols ">
+                @foreach ($posts as $post)
+                    <div class="col-3">
+                        <div class="card">
+                            {{-- <img class="card-img-top" src="..." alt="Card image cap"> --}}
+                            <div class="card-body">
+                                <h5 class="card-title">{{$post->title}}</h5>
+                                <p class="card-text">{{Str::substr($post->content, 0 , 60)}}</p>
+                            </div>
+                            <div class="card-body">
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    @endforeach
+                </div>
         </div>
     </section>
 @endsection
