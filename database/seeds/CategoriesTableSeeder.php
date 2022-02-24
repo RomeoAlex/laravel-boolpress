b02bfbe0a59c00c2b3f57a6ae89875433c24d4f3
RomeoAlex <romeo.alessandro17@gmail.com>
2022-02-24 18:13:38 +0100

    utilizzato array con le categorie per popolare la tebella e seed tramite CLI

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Category;
+// per slug bisogna importare la funzione di supporto
+use Illuminate\Support\Str;
 class CategoriesTableSeeder extends Seeder
 {
     /**
@@ -20,5 +22,12 @@ class CategoriesTableSeeder extends Seeder
             'scienza',
             'tecnologia'
         ];
+        // con foreach andrò a riempire la tabella
+        foreach ($categories as $category) {
+            $new_category = new Category();
+            $new_category->name = $category;
+            $new_category->slug = Str::slug($category);
+            $new_category->save();
+        }
     }
 }
