0874217664fd48255b6e7eec9f37ca3aa4b6ff42
RomeoAlex <romeo.alessandro17@gmail.com>
2022-02-24 19:34:36 +0100

    migrazione effettuata per la foreign key sui post||ATTENZIONE LA MIGRAZIONE NON PERMETTE COMMENTI ALL'INTERNO DELLA FUNZIONE UP O LE INFORMAZIONI DI MYSQL INVIATE SARANNO MALE INTERPRETATE DAL DATABASE

--- database/migrations/2022_02_24_171910_add_category_id_to_posts.php
+++ database/migrations/2022_02_24_171910_add_category_id_to_posts.php
@@ -14,19 +14,33 @@ class AddCategoryIdToPosts extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            //aggiungo la foreign key, la mettiamo nullable perchè non la vogliamo obbligatoria nel caso specifico
+            $table->unsignedBigInteger('category_id')->nullable()->after('slug');
+
+            $table->foreign('category_id')
+            ->references('id')
+            ->on('categories')
+            ->onDelete('set null');
         });
     }
+    // nel caso della cancellazione dobbiamo specificare cosa deve fare mysql per non "rompere" la tabella
+    // per eliminare tutte le confluenze della foreign
+    // ->onDelete('cascade');
+    // rimette a default tutte le relazioni DA VEDERE SULLA DOCUMENTAZIONE e DEVE ESSERE ASSIMILABILE DALLA TABELLA
+    // ->onDelete('default');
 
     /**
      * Reverse the migrations.
      *
      * @return void
      */
+    // 1 --importante cancellare la colonna in questione!! BISOGNA ANCHE CANCELLARE LA RELAZIONE IN UP
+    // 2-- perciò eliminiamo anche la relazione
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->dropForeign('posts_category_id_foreign');
+            $table->dropColumn('category_id');
         });
     }
 }
