e044905b0192a04e47052788b0f2a856a37fd7f8
RomeoAlex <romeo.alessandro17@gmail.com>
2022-03-02 22:42:57 +0100

    preparato i dati del model post da inviare tramite json

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,10 +4,29 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
    public function index() {
-       dd('test per rotta');
+    // prendo i post dal model   
+    $posts = Post::all();
+    // rimando i dati a json per vue, controllo tramite postman
+    // return response()->json($posts);
+    
+    // $response_array = [
+    //     // variabile per notificare il successo dell'operazione a schermo in caso di false si riporta su una pagina di manutenzione
+    //     'success' => true,
+    //     'results' => $posts
+    // ];
+    
+    // return response()->json($response_array);
+
+    
+    // 2 tipologie differenti 
+    return response()->json([
+        'success' => true,
+        'results' => $posts
+    ]);
    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
 // rotta per i post 
 Route::get('/posts', 'Api\PostController@index');
